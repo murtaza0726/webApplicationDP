a8f5a24270b276f57c3b5e044cc210e462528382
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2022-04-06 23:20:19 -0400

    first commit

---
+++ myApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib import admin
+#from django.conf import settings
+#from django.conf.urls.static import static
+from myApp import views
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('group', views.group, name="group"),
+    path('about', views.about, name="about"),
+    path('chart', views.dome, name="dome"),
+    #path('chart', include('project.urls')),
+]
---
+++ myApp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import urllib,json
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def group(request):
+    return render(request, 'group.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dome(request):
+    url = 'https://mydbdeployment.herokuapp.com/health'
+    res = urllib.request.urlopen(url)
+    data = json.loads(res.read())
+
+    labels=[]
+    chartdata=[]
+    for City in data:
+        labels.append(City['City'])
+        chartdata.append(City['ObesityLevels_percent'])
+    return render(request, 'dome.html', {'data':data, 'labels':labels, 'chartdata':chartdata})
